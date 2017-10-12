3ed6fc9e7dc3f62443c59f290ecae0cbf6a92936
Michael Gruner <michael.gruner@ridgerun.com>
2017-10-11 19:01:14 -0600

    Add script to create recording pipeline

---
+++ create_recording.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "========================"
+echo "Creating recording pipeline"
+echo "========================"
+
+gst-client pipeline_delete recording
+gst-client pipeline_create recording \
+interpipesrc listen-to=camera accept-events=false accept-eos-event=false enable-sync=false ! \
+omxh264enc ! h264parse ! \
+mpegtsmux ! \
+filesink location=/tmp/gstd_30min_server_recording.ts
