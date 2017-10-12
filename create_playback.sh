018556967c6375d3eccab4678947ce5747c4808e
Michael Gruner <michael.gruner@ridgerun.com>
2017-10-12 08:46:39 -0600

    Add script to create playback pipeline

---
+++ create_playback.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "========================"
+echo "Creating playback pipeline"
+echo "========================"
+
+gst-client pipeline_delete playback
+gst-client pipeline_create playback \
+filesrc location=/tmp/gstd_30min_server_recording.ts ! \
+tsdemux ! \
+h264parse ! \
+omxh264dec ! \
+nvvidconv ! video/x-raw,format=I420,width=1920,height=1080 ! \
+interpipesink caps=video/x-raw,format=I420,width=1920,height=1080 \
+name=playback sync=true forward-events=false forward-eos=false
