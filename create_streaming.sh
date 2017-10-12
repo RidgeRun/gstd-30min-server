75749cd7dab846e06c1c526b5c10dad3d606212c
Michael Gruner <michael.gruner@ridgerun.com>
2017-10-12 13:08:41 -0600

    Add script to create streaming pipeline

---
+++ create_streaming.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "========================"
+echo "Creating streaming pipeline"
+echo "========================"
+
+gst-client pipeline_delete streaming
+gst-client pipeline_create streaming \
+interpipesrc name=src format=time listen-to=camera accept-events=false accept-eos-event=false enable-sync=false \
+caps="video/x-raw,width=1920,height=1080,format=I420,framerate=30/1" ! \
+omxvp8enc iframeinterval=10 ! \
+video/x-vp8,mapping=/ridgerun ! \
+rtspsink service=12345
