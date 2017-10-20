aa8df157b7b1a500cd61b8d5d73b7559553ac96d
Michael Gruner <michael.gruner@ridgerun.com>
2017-10-20 14:00:44 -0600

    Add timout to snapshot EOS

--- 06_play_snapshot.sh
+++ 06_play_snapshot.sh
@@ -5,6 +5,7 @@ echo "Playing snapshot pipeline"
 echo "========================"
 
 gst-client bus_filter snapshot eos
+gst-client bus_timeout snapshot 1000000000
 gst-client pipeline_play snapshot
 gst-client bus_read snapshot
 gst-client pipeline_stop snapshot
